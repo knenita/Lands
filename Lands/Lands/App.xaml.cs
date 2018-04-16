786785774986d43a50fd1eb77d94bc8d6f5ddd58
knenita <dianarussi@yahoo.com>
2018-04-15 22:19:19 -0500

    Create Login Page video 1 to 5

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Lands
+{
+    using Views;
+    using Xamarin.Forms;
+    public partial class App : Application
+	{
+        #region Constructors 
+		public App ()
+		{
+			InitializeComponent();
+
+			this.MainPage = new NavigationPage( new LoginPage());
+		}
+        #endregion
+
+        #region Methods
+        // Este metodo se ejecuta una vez cuando la app se ejecuta
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+        // Aplicacion se duerme
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+        //Volver a la aplicacion 
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+        #endregion
+
+	}
+}
---
+++ Lands/Lands/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infraestructure
+{
+    //referenciar viewModels
+    using ViewModels;
+
+
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}

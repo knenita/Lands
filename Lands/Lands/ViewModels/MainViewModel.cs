4c62cbe4536f86a1b05b288908f80a50736d26de
knenita <dianarussi@yahoo.com>
2018-04-20 23:12:57 -0500

    Login View Model

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,5 +6,20 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModel
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }     
+        #endregion
+
     }
 }

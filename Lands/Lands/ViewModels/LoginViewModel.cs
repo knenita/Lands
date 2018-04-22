c831d6650383088ec48d3706dcd1c2012eb6c437
knenita <dianarussi@yahoo.com>
2018-04-22 00:25:58 -0500

    Login Page NotifyPropertyChanged

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,8 +1,21 @@
 ﻿namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
-    public class LoginViewModel
+    using Xamarin.Forms;
+
+    public class LoginViewModel: BaseViewModel
     {
+        //#region Events
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //#endregion
+
+        #region Atributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email
         {
@@ -12,35 +25,86 @@
 
         public string Password
         {
-            get;
-            set;
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
         }
         public bool IsRemembered
         {
             get;
             set;
         }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Commands
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password",
+                    "Accept");
+                return;
+            }
+
+            this.isRunning = true;
+            this.IsEnabled = false;
+            if (this.Email != "dianarussi@yahoo.com" || this.Password != "1234")
+            {
+                this.isRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "Error",
+                   "Email or password incorrect.",
+                   "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+            this.isRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                  "OK",
+                  "Fuck yeahh.",
+                  "Accept");
+
         }
         #endregion
-
         #region Constructors
         public LoginViewModel ()
         {
             this.IsRemembered = true;
-        }
-        #endregion
+            this.isEnabled = true;
+        }        
     }
+        #endregion
 }
+
